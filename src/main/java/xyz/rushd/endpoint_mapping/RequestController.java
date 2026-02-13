ab600fd4996c643456950267717200f5e8757a8e
rgm <git@rushd.xyz>
2026-02-13 16:40:27 +0000

    Added greetByName endpoint to test @PathVariable

--- src/main/java/xyz/rushd/endpoint_mapping/RequestController.java
+++ src/main/java/xyz/rushd/endpoint_mapping/RequestController.java
@@ -1,15 +1,20 @@
 package xyz.rushd.endpoint_mapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class RequestController {
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello World!";
+  }
 
-    @GetMapping("/hello")
-    public String hello() {
-      return "Hello World!";
-    }
+  @GetMapping("/greet/{name}")
+  public String greetByName(@PathVariable String name) {
+    return "Hello " + name;
+  }
 }
