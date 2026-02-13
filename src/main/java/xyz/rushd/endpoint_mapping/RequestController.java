078222dfbc746a22dfa7afcc32ae15261f615c4f
rgm <git@rushd.xyz>
2026-02-13 16:34:13 +0000

    Added hello endpoint to test @GetMapping

--- src/main/java/xyz/rushd/endpoint_mapping/RequestController.java
+++ src/main/java/xyz/rushd/endpoint_mapping/RequestController.java
@@ -1,5 +1,15 @@
 package xyz.rushd.endpoint_mapping;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class RequestController {
 
+    @GetMapping("/hello")
+    public String hello() {
+      return "Hello World!";
+    }
 }
